a0048d82b4b60e56b06a317fa39fa003fb8d9f91
Shua Myers <sym300@gmail.com>
2018-03-20 17:33:56 +0200

    get gImgs func

--- js/mene.js
+++ js/mene.js
@@ -1,9 +1,37 @@
 'use strict';
 console.log('sprint 2');
 
+var gId=1;
+var gImgs;
 
+// var gMeme = {
+//     selectedImgId: 5,
+//     txts:[{
+//         line: 'I never eat Falafel',
+//         size: 20,
+//         align: 'left',
+//         color: 'red'
+//     }]} 
 
-
-function handleImg (elImg) {
-    console.log (elImg);
+function init(){
+    gImgs =getImgs();
+    // renderImgs(gImgs); 
 }
+
+function getImgs(){
+    var imgs = [];
+
+
+    imgs.push(getImg(001,['funny','happy']));
+    imgs.push(getImg(002,['funny','happy']));
+    imgs.push(getImg(003,['funny','happy']));
+    
+    return imgs;
+}
+
+function getImg(imgId,keywords){
+    return {
+        id:gId++,
+        url:'../img/'+imgId+'.jpg',
+        keywords:keywords
+    }}
